6afd75d5f1adb8b23400f24a9ed3411cf97ce0c0
MishaRysev <r0botbob@yandex.ru>
2026-02-14 00:33:47 +0000

    ok lets go

---
+++ tasks/rysev_m_most_same_elements/mpi/src/ops_mpi.cpp
@@ -0,0 +1,95 @@
+#include "example_processes/mpi/include/ops_mpi.hpp"
+
+#include <mpi.h>
+
+#include <algorithm>
+#include <cmath>
+#include <utility>
+#include <vector>
+
+#include "example_processes/common/include/common.hpp"
+#include "util/include/util.hpp"
+
+namespace rysev_m_max_adjacent_diff {
+
+struct DiffPair {
+  int diff;
+  int first;
+  int second;
+};
+
+RysevMMaxAdjacentDiffMPI::RysevMMaxAdjacentDiffMPI(const InType &in) {
+  SetTypeOfTask(GetStaticTypeOfTask());
+  GetInput() = in;
+  GetOutput() = std::make_pair(0, 0);
+}
+
+bool RysevMMaxAdjacentDiffMPI::ValidationImpl() {
+  return (GetInput().size() >= 2);
+}
+
+bool RysevMMaxAdjacentDiffMPI::PreProcessingImpl() {
+  GetOutput() = std::make_pair(0, 0);
+  return true;
+}
+
+bool RysevMMaxAdjacentDiffMPI::RunImpl() {
+  int rank = 0;
+  int size = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  const auto& input = GetInput();
+  size_t n = input.size();
+
+  if (n < 2) {
+    return false;
+  }
+
+  size_t total_pairs = n - 1;
+  size_t pairs_per_process = total_pairs / size;
+  size_t remainder = total_pairs % size;
+
+  size_t start_pair = rank * pairs_per_process + std::min(static_cast<size_t>(rank), remainder);
+  size_t end_pair = start_pair + pairs_per_process + (static_cast<size_t>(rank) < remainder ? 1 : 0);
+
+  DiffPair local_best = { -1, 0, 0 };
+  
+  for (size_t i = start_pair; i < end_pair && i < total_pairs; ++i) {
+    int diff = std::abs(input[i + 1] - input[i]);
+    if (diff > local_best.diff) {
+      local_best.diff = diff;
+      local_best.first = input[i];
+      local_best.second = input[i + 1];
+    }
+  }
+
+  DiffPair global_best = { -1, 0, 0 };
+  
+  MPI_Op max_diff_op;
+  MPI_Op_create(
+      [](void *invec, void *inoutvec, int *len, MPI_Datatype *datatype) {
+        DiffPair *in = static_cast<DiffPair*>(invec);
+        DiffPair *inout = static_cast<DiffPair*>(inoutvec);
+        for (int i = 0; i < *len; i++) {
+          if (in[i].diff > inout[i].diff) {
+            inout[i] = in[i];
+          }
+        }
+      },
+      1, &max_diff_op);
+
+  MPI_Allreduce(&local_best, &global_best, 1, MPI_BYTE, max_diff_op, MPI_COMM_WORLD);
+  MPI_Op_free(&max_diff_op);
+
+  GetOutput() = std::make_pair(global_best.first, global_best.second);
+  
+  MPI_Barrier(MPI_COMM_WORLD);
+  return true;
+}
+
+bool RysevMMaxAdjacentDiffMPI::PostProcessingImpl() {
+  return true;
+}
+
+}  // namespace rysev_m_max_adjacent_diff
---
+++ tasks/rysev_m_most_same_elements/tests/functional/main.cpp
@@ -0,0 +1,132 @@
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <cstddef>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+#include "example_processes/common/include/common.hpp"
+#include "example_processes/mpi/include/ops_mpi.hpp"
+#include "example_processes/seq/include/ops_seq.hpp"
+#include "util/include/func_test_util.hpp"
+
+namespace rysev_m_max_adjacent_diff {
+
+class MaxAdjacentDiffFuncTest : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
+ public:
+  static std::string PrintTestParam(const TestType &test_param) {
+    return "test_" + std::to_string(std::get<0>(test_param)) + "_" + std::get<1>(test_param);
+  }
+
+ protected:
+  void SetUp() override {
+    TestType params = std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kTestParams)>(GetParam());
+    int test_id = std::get<0>(params);
+    
+    switch (test_id) {
+      case 1:
+        input_data_ = {1, 2, 3, 4, 5};
+        expected_output_ = std::make_pair(1, 2);
+        break;
+        
+      case 2:
+        input_data_ = {10, 10, 10, 100, 100};
+        expected_output_ = std::make_pair(10, 100);
+        break;
+        
+      case 3:
+        input_data_ = {100, 90, 80, 70, 60};
+        expected_output_ = std::make_pair(100, 90);
+        break;
+        
+      case 4:
+        input_data_ = {-10, -5, 0, 5, 10};
+        expected_output_ = std::make_pair(-10, -5);
+        break;
+        
+      case 5:
+        input_data_ = {1, 100, 2, 99, 3};
+        expected_output_ = std::make_pair(1, 100);
+        break;
+        
+      case 6:
+        input_data_ = {42, 100};
+        expected_output_ = std::make_pair(42, 100);
+        break;
+        
+      case 7:
+        input_data_ = {5, 5, 5, 5, 5};
+        expected_output_ = std::make_pair(5, 5);
+        break;
+        
+      case 8:
+        input_data_ = {1, 2, 3, 100, 4, 5, 6};
+        expected_output_ = std::make_pair(3, 100);
+        break;
+        
+      case 9:
+        input_data_ = {10, 20, 30, 40, 50, 1000};
+        expected_output_ = std::make_pair(50, 1000);
+        break;
+        
+      case 10:
+        input_data_ = {1, 100, 2, 3, 100, 4};
+        expected_output_ = std::make_pair(1, 100);
+        break;
+        
+      default:
+        input_data_ = {0, 0};
+        expected_output_ = std::make_pair(0, 0);
+        break;
+    }
+  }
+
+  bool CheckTestOutputData(OutType &output_data) final {
+    return expected_output_.first == output_data.first && 
+           expected_output_.second == output_data.second;
+  }
+
+  InType GetTestInputData() final {
+    return input_data_;
+  }
+
+ private:
+  InType input_data_;
+  OutType expected_output_;
+};
+
+namespace {
+
+TEST_P(MaxAdjacentDiffFuncTest, VectorMaxAdjacentDiff) {
+  ExecuteTest(GetParam());
+}
+
+const std::array<TestType, 10> kTestParams = {
+    std::make_tuple(1, "increasing"),
+    std::make_tuple(2, "single_jump"),
+    std::make_tuple(3, "decreasing"),
+    std::make_tuple(4, "negative"),
+    std::make_tuple(5, "alternating"),
+    std::make_tuple(6, "minimal"),
+    std::make_tuple(7, "constant"),
+    std::make_tuple(8, "max_in_middle"),
+    std::make_tuple(9, "max_at_end"),
+    std::make_tuple(10, "multiple_max")
+};
+
+const auto kTestTasksList =
+    std::tuple_cat(ppc::util::AddFuncTask<RysevMMaxAdjacentDiffMPI, InType>(kTestParams, PPC_SETTINGS_example_processes),
+                   ppc::util::AddFuncTask<RysevMMaxAdjacentDiffSEQ, InType>(kTestParams, PPC_SETTINGS_example_processes));
+
+const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
+
+const auto kTestName = MaxAdjacentDiffFuncTest::PrintFuncTestName<MaxAdjacentDiffFuncTest>;
+
+INSTANTIATE_TEST_SUITE_P(MaxAdjacentDiffTests, MaxAdjacentDiffFuncTest, kGtestValues, kTestName);
+
+}  // namespace
+
+}  // namespace rysev_m_max_adjacent_diff
