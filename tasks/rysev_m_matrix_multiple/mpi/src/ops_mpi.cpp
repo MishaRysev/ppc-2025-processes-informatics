edaacc3628df0dbecf7ef34bdd9d2d00ee594d44
MishaRysev <r0botbob@yandex.ru>
2026-02-23 18:14:33 +0000

    asfsdf

--- tasks/rysev_m_matrix_multiple/mpi/src/ops_mpi.cpp
+++ tasks/rysev_m_matrix_multiple/mpi/src/ops_mpi.cpp
@@ -42,16 +42,17 @@ bool RysevMMatrMulMPI::PreProcessingImpl() {
   }
 
   MPI_Bcast(&size_, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  return true;
-}
 
-bool RysevMMatrMulMPI::RunImpl() {
   if (rank_ != 0) {
     A_.resize(size_ * size_);
     B_.resize(size_ * size_);
     C_.resize(size_ * size_);
   }
 
+  return true;
+}
+
+bool RysevMMatrMulMPI::RunImpl() {
   MPI_Bcast(B_.data(), size_ * size_, MPI_INT, 0, MPI_COMM_WORLD);
 
   std::vector<int> send_counts(num_procs_);
@@ -70,16 +71,11 @@ bool RysevMMatrMulMPI::RunImpl() {
 
   local_rows_ = send_counts[rank_] / size_;
 
-  if (local_rows_ > 0) {
-    local_A_.resize(local_rows_ * size_);
-    local_C_.assign(local_rows_ * size_, 0);
-  } else {
-    local_A_.clear();
-    local_C_.clear();
-  }
+  local_A_.resize(send_counts[rank_]);
+  local_C_.assign(local_rows_ * size_, 0);
 
-  MPI_Scatterv(rank_ == 0 ? A_.data() : nullptr, send_counts.data(), displs.data(), MPI_INT,
-               local_rows_ > 0 ? local_A_.data() : nullptr, local_rows_ * size_, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Scatterv(rank_ == 0 ? A_.data() : nullptr, send_counts.data(), displs.data(), MPI_INT, local_A_.data(),
+               send_counts[rank_], MPI_INT, 0, MPI_COMM_WORLD);
 
   if (local_rows_ > 0) {
     for (int i = 0; i < local_rows_; ++i) {
@@ -104,8 +100,8 @@ bool RysevMMatrMulMPI::RunImpl() {
     offset += recv_counts[i];
   }
 
-  MPI_Gatherv(local_rows_ > 0 ? local_C_.data() : nullptr, local_rows_ * size_, MPI_INT,
-              rank_ == 0 ? C_.data() : nullptr, recv_counts.data(), recv_displs.data(), MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Gatherv(local_C_.data(), local_rows_ * size_, MPI_INT, rank_ == 0 ? C_.data() : nullptr, recv_counts.data(),
+              recv_displs.data(), MPI_INT, 0, MPI_COMM_WORLD);
 
   return true;
 }
