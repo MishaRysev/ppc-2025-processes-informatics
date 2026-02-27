9d314c6977588cdda135cf4c48a845af356e571e
MishaRysev <r0botbob@yandex.ru>
2026-02-27 23:00:31 +0000

    iuggiuygiy

--- tasks/rysev_m_shell_sort_simple_merge/tests/performance/main.cpp
+++ tasks/rysev_m_shell_sort_simple_merge/tests/performance/main.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
-#include <random>
+#include <algorithm>
+#include <vector>
 
 #include "rysev_m_shell_sort_simple_merge/common/include/common.hpp"
 #include "rysev_m_shell_sort_simple_merge/mpi/include/ops_mpi.hpp"
@@ -12,22 +13,19 @@ namespace rysev_m_shell_sort_simple_merge {
 class ShellSortPerfTest : public ppc::util::BaseRunPerfTests<InType, OutType> {
   const int kSize_ = 10000;
   InType input_data_{};
+  OutType expected_data_;
 
   void SetUp() override {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(1, 10000);
-
     input_data_.resize(kSize_);
     for (int i = 0; i < kSize_; ++i) {
-      input_data_[i] = dis(gen);
+      input_data_[i] = kSize_ - i;
     }
+    expected_data_ = input_data_;
+    std::sort(expected_data_.begin(), expected_data_.end());
   }
 
   bool CheckTestOutputData(OutType &output_data) final {
-    std::vector<int> expected = input_data_;
-    std::sort(expected.begin(), expected.end());
-    return expected == output_data;
+    return expected_data_ == output_data;
   }
 
   InType GetTestInputData() final {
